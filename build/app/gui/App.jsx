cc7ef5934d543949fd1ac04b7468b3e4735b1d10
Dave Seah <david@davidseah.com>
2018-02-20 17:46:58 -0500

    update index.html and App.jsx to demonstrate Bootstrap 4

--- build/app/gui/App.jsx
+++ build/app/gui/App.jsx
@@ -5,7 +5,7 @@ export default class App extends React.Component {
 	render() {
 		return (
 			<div id="content">
-			<h1>NetCreate AppShell</h1>
+				Loaded from app/gui/App.jsx
 			</div>
 		);
 	}
