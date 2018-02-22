2b37c15ff17d55fc694588802106e70b20f2ced3
Dave Seah <david@davidseah.com>
2018-02-22 17:06:11 -0500

    add reactstrap, convert AppContainer to use reactstrap components

--- build/app/component/AppContainer.jsx
+++ build/app/component/AppContainer.jsx
@@ -1,33 +1,69 @@
 import React from 'react';
+import {
+	Collapse,
+	Navbar,
+	NavbarToggler,
+	NavbarBrand,
+	Nav,
+	NavItem,
+	NavLink,
+	UncontrolledDropdown,
+	DropdownToggle,
+	DropdownMenu,
+	DropdownItem } from 'reactstrap';
+
 import AppDefault from 'view/AppDefault';
 
-export default class App extends React.Component {
 
+export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.toggle = this.toggle.bind(this);
+		this.state = {
+			isOpen: false
+		};
+	}
+	toggle() {
+		this.setState({
+			isOpen: !this.state.isOpen
+		});
+	}
 	render() {
 		return (
 			<div>
-			<nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-				<a className="navbar-brand" href="#">NetCreate</a>
-				<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-					<span className="navbar-toggler-icon"></span>
-				</button>
-				<div className="collapse navbar-collapse" id="navbarCollapse">
-					<ul className="navbar-nav mr-auto">
-						<li className="nav-item active">
-							<a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="#">Link</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="#">Link</a>
-						</li>
-					</ul>
-				</div>
-			</nav>
-			<AppDefault/>
+				<Navbar fixed="top" color="dark" dark expand="md">
+					<NavbarBrand href="#">NetCreate</NavbarBrand>
+					<NavbarToggler onClick={this.toggle} />
+					<Collapse isOpen={this.state.isOpen} navbar>
+						<Nav className="ml-auto" navbar>
+							<NavItem>
+								<NavLink href="#">About</NavLink>
+							</NavItem>
+							<NavItem>
+								<NavLink href="#">Github</NavLink>
+							</NavItem>
+							<UncontrolledDropdown nav inNavbar>
+								<DropdownToggle nav caret>
+									Dev Tests
+								</DropdownToggle>
+								<DropdownMenu >
+									<DropdownItem>
+										Test 1
+									</DropdownItem>
+									<DropdownItem>
+										Test 2
+									</DropdownItem>
+									<DropdownItem divider />
+									<DropdownItem>
+										Test 3
+									</DropdownItem>
+								</DropdownMenu>
+							</UncontrolledDropdown>
+						</Nav>
+					</Collapse>
+				</Navbar>
 			</div>
 		);
 	}
-
 }
