f6c2dbb21181a393fcfe87be209efc487e5e15c4
Dave Seah <david@davidseah.com>
2018-06-21 02:12:05 -0400

    ds-acd-rf-review: clean up documentation, format lifecycle, remove vestigial code

--- build/app/view/autocompletedemo/AutoCompleteDemo.jsx
+++ build/app/view/autocompletedemo/AutoCompleteDemo.jsx
@@ -1,66 +1,38 @@
 /*//////////////////////////////// ABOUT \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*\
 
+    AUTO COMPLETE DEMO
 
-    Auto Complete Demo
-    ==================
-
-    The basic structure of the app looks like this:
+    The basic React Component structure of the app looks like this:
 
         AutoCompleteDemo
-        |
         +- NodeSelector
-        |  |
         |  +- NodeDetail
-        |  |
         |  +- AutoComplete
-        |  |  |
         |  |  +- AutoSuggest
-        |  |
         |  +- EdgeEntry
-        |     |
         |     +- *AutoComplete (for Target Node)*
-        |
         +- NetGraph
-           |
            +- D3SimpleNetGraph
-              |
               +- D3
 
-
-
-    # AutoCompleteDemo.jsx
-
-    `AutoCompleteDemo.jsx` is the root element.  It is essentially a dumb wrapper
-    for the key app elements; NodeSelector and NetGraph.
+    `AutoCompleteDemo` is the root element. It is a wrapper for the key app
+    elements `NodeSelector` and `NetGraph`.
 
     It does not do any data or event handling.  Those are handled individually
-    by the repsective Components.
+    by the respective Components.
 
-    *   All state is maintained in autocomplete-logic.jsx.
-    *   It handles events from NodeSelector, EdgeEntry, and NetGraph components
-        and passes data and upates across them.
+  * All state is maintained in `autocomplete-logic.js`
+  * It handles events from NodeSelector, EdgeEntry, and NetGraph components
+      and passes data and upates across them.
 
-
-    *PROPS*
-
-                            (none)
-
-
-    *STATE*
-
-                            (none)
-
-
-    *EVENTS*
-
-                            (none)
-
-
+    PROPS  ... (none)
+    STATE  ... (none)
+    EVENTS ... (none)
 
 \*\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ * //////////////////////////////////////*/
 
-var DBG = false;
-
+/// DEBUG SWITCHES ////////////////////////////////////////////////////////////
+var   DBG          = false;
 
 /// LIBRARIES /////////////////////////////////////////////////////////////////
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -73,76 +45,73 @@ const ReactStrap   = require('reactstrap');
 /// NEW SIGNLING SYSTEM LIBRARIES /////////////////////////////////////////////
 const UNISYS       = require('system/unisys');
 var   UDATA        = null; // set in constructor
-      // start UNISYS
-      UNISYS.SystemInitialize( module.id );
 
+/// INITIALIZE MODULE /////////////////////////////////////////////////////////
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+// MUST start UNISYS before any REACT lifecycle events fire, because we need
+// to make sure UNISYS is ready to accept handler registration This call
+// only happens ONCE in the ROOT COMPONENT or ROOT MODULE of our webapp.
+UNISYS.SystemInitialize( module.id );
+
+/** END INIT *****************************************************************/
 
 /// REACT COMPONENT ///////////////////////////////////////////////////////////
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-/// export a class object for consumption by brunch/require
 class AutoCompleteDemo extends React.Component {
+
   constructor () {
-    super()
-    this.state = {
-      data:                    {},    // nodes and edges data object
-      selectedSourceNode:      {},
-      selectedTargetNode:      {},
+    super();
+    // UDATA is the interface to unisys messaging and state features
+    UDATA = UNISYS.NewDataLink(this);
+  }
+
+/// REACT LIFECYCLE METHODS ///////////////////////////////////////////////////
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+/*/ This is the root component, so this fires after all subcomponents have
+    been fully rendered by render().
+/*/ componentDidMount () {
+      // start UNISYS lifecycle after REACT is ready
+      (async () => {
+        await UNISYS.EnterApp();  // INITIALIZE, LOADASSETS
+        await UNISYS.SetupRun();  // RESET, CONFIGURE, START
+      })();
+      // NOTE: see unisys-lifecycle.js for more run modes
     }
-
-    /*NEWCODE*/
-    console.group(module.id);
-    UDATA = UNISYS.NewDataLink( this );
-    UDATA.Call('ACD_CONSTRUCT');
-    console.groupEnd();
-    /*NEWCODE END*/
-
-  }
-
-  /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  /// REACT LIFECYCLE
-  ///
-  componentDidMount () {
-    // kickoff initialization stage by stage
-    (async () => {
-      await UNISYS.EnterApp();
-      await UNISYS.SetupRun();
-    })();
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Auto Complete Demo</h1>
-        <p>This demonstrates how a d3 force simulation, a node selection input
-        field, and a node viewer might be encapsulated into react components
-        and pass data back and forth to each other.</p>
-        <p>INSTRUCTIONS: Type in the "Nodes" input field to highlight nodes or
-        a node around to see how it's linked to other nodes.</p>
-        <div style={{display:'flex', flexFlow:'row nowrap',
-             width:'100%', height:'100%'}}>
-          <div id="left" style={{backgroundColor:'#E0ffff',flex:'1 0 auto',maxWidth:'300px',padding:'10px'}}>
-            <div style={{display:'flex', flexFlow:'column nowrap'}}>
-              <div style={{flexGrow:1}}>
-                <h3>Nodes (RF)</h3>
-              </div>
-              <div>
-                <NodeSelector/>
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+/*/ Define the component structure of the web application
+/*/ render() {
+      return (
+        <div>
+          <h1>Auto Complete Demo</h1>
+          <p>This demonstrates how a d3 force simulation, a node selection input
+          field, and a node viewer might be encapsulated into react components
+          and pass data back and forth to each other.</p>
+          <p>INSTRUCTIONS: Type in the "Nodes" input field to highlight nodes or
+          a node around to see how it's linked to other nodes.</p>
+          <div style={{display:'flex', flexFlow:'row nowrap',
+               width:'100%', height:'100%'}}>
+            <div id="left" style={{backgroundColor:'#E0ffff',flex:'1 0 auto',maxWidth:'300px',padding:'10px'}}>
+              <div style={{display:'flex', flexFlow:'column nowrap'}}>
+                <div style={{flexGrow:1}}>
+                  <h3>Nodes (RF)</h3>
+                </div>
+                <div>
+                  <NodeSelector/>
+                </div>
               </div>
             </div>
-          </div>
-          <div id="middle" style={{backgroundColor:'#fcfcfc', flex:'3 0 auto', padding:'10px'}}>
-            <NetGraph/>
-          </div>
-          <div id="right" style={{backgroundColor:'#ffffE0', flex:'1 0 auto', padding:'10px'}}>
-            <h3>Edges</h3>
+            <div id="middle" style={{backgroundColor:'#fcfcfc', flex:'3 0 auto', padding:'10px'}}>
+              <NetGraph/>
+            </div>
+            <div id="right" style={{backgroundColor:'#ffffE0', flex:'1 0 auto', padding:'10px'}}>
+              <h3>Edges</h3>
+            </div>
           </div>
         </div>
-      </div>
-    )
-  }
-}
-
-
+      ); // end return
+    } // end render()
+  } // end class AutoCompleteDemo
 
 /// EXPORT REACT COMPONENT ////////////////////////////////////////////////////
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
