da621f8163645568f9e57ee6b39f3da613fbff3e
Ben Loh <ben@inquirium.net>
2018-08-22 22:20:06 -0700

    Fix Help to the right and float on top of graph.

--- build/app/view/autocompletedemo/components/Help.jsx
+++ build/app/view/autocompletedemo/components/Help.jsx
@@ -54,16 +54,24 @@ class Help extends UNISYS.Component {
 /*/ render () {
       return (
         <div className="help"
-             style={{maxHeight:'50vh',overflow:'scroll',float:'right'}}>
+             style={{maxHeight:'50vh',maxWidth:'50%',overflow:'scroll',
+                     position:'fixed',right:'10px',zIndex:100
+             }}>
           <Button size="sm" outline
             style={{float:'right'}}
             onClick={this.onToggleExpanded}
           >{this.state.isExpanded ? "Hide Help" : "Help"}</Button>
           <div hidden={!this.state.isExpanded}
-            style={{backgroundColor:'#eee',padding:'10px'}}>
+            style={{backgroundColor:'rgba(240,240,240,0.85)',padding:'10px'}}>
             <h1>Navigation</h1>
             <ul>
-              <li>Zoom -- mousewheel or two fingers up/down on trackpad</li>
+              <li>Zoom --
+                <ul>
+                  <li>mouse -- use mousewheel</li>
+                  <li>trackpad -- two fingers up/down</li>
+                  <li>tablet -- two fingers pinch</li>
+                </ul>
+              </li>
               <li>Pan -- drag empty space</li>
             </ul>
             <h1>Nodes</h1>
@@ -73,6 +81,9 @@ class Help extends UNISYS.Component {
             </ul>
             <h1>Edges</h1>
             <ul>
+              <li>Create -- To create an edge, first select the source node, then
+              click "Add New Edge".</li>
+              <li>Select -- To select an edge, select either of the nodes it is attached to.</li>
               <li>Editing -- To change the source or target of an existing edge,
               delete it and create a new one.</li>
             </ul>
