cdfb27cc72c388c2bf22179eb60c10532fc56b11
Ben Loh <ben@inquirium.net>
2021-11-18 17:22:02 -0800

    export: Add export-logic to handle export request.

---
+++ build/app/view/netcreate/export-logic.js
@@ -0,0 +1,199 @@
+/*//////////////////////////////// ABOUT \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*\
+
+  * Export Logic
+
+\*\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ * //////////////////////////////////////*/
+
+const DBG = false;
+
+/// LIBRARIES /////////////////////////////////////////////////////////////////
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+const UNISYS = require("unisys/client");
+
+/// INITIALIZE MODULE /////////////////////////////////////////////////////////
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+var MOD = UNISYS.NewModule(module.id);
+var UDATA = UNISYS.NewDataLink(MOD);
+
+/// UTILITIES /////////////////////////////////////////////////////////////////
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+function m_formatDate(date) {
+  // wrap in quotes because time includes a comma
+  if (date) return `"${new Date(date).toUTCString()}"`;
+  return '';
+}
+
+/// IMPORT / EXPORT METHODS ///////////////////////////////////////////////////
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+/*/ Returns an array of values for a given node record
+    e.g. [1,'Tacitus','Person',...]
+/*/
+function m_getNodeValues(node, keys) {
+  const RESULT = [];
+  keys.forEach(key => {
+    // If the key is an object, recurse
+    // eslint-disable-next-line prefer-reflect
+    if (Object.prototype.toString.call(key) === '[object Object]') {
+      const subKeys = Object.keys(key); // can have multiple subKeys
+      subKeys.forEach(k => {
+        RESULT.push( m_getNodeValues(node[k], key[k]) );
+      });
+    }
+    // Special Data Handling
+    // -- DATE
+    if (['created', 'updated'].includes(key)) {
+      RESULT.push(m_formatDate(node[key]));
+      return;
+    }
+    // Else, normal processing
+    if (node.hasOwnProperty(key)) RESULT.push(`"${node[key]}"`); // enclose in quotes to support commas
+  })
+  return RESULT;
+}
+
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+/*/ Returns an array of node records
+    e.g. [[<tacitus>], [<marc antony>], ...]
+/*/
+function m_GenerateNodesArray(nodes, nodekeys) {
+  /// Define Node KEYS
+  const nodesArr = [];
+  nodes.forEach(n => nodesArr.push(m_getNodeValues(n, nodekeys)));
+  return nodesArr;
+}
+
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+/*/ Returns an array of values for a given node record
+    e.g. [1,'Tacitus','Person',...]
+/*/
+function m_getEdgeValues(edge, keys) {
+  const RESULT = [];
+  keys.forEach(key => {
+    // If the key is an object, recurse
+    // eslint-disable-next-line prefer-reflect
+    if (Object.prototype.toString.call(key) === '[object Object]') {
+      const subKeys = Object.keys(key); // can have multiple subKeys
+      subKeys.forEach(k => {
+        RESULT.push( m_getNodeValues(edge[k], key[k]) );
+      });
+    }
+    // Special Data Handling
+    // -- SOURCE / TARGET
+    if (['source', 'target'].includes(key)) {
+      RESULT.push(edge[key].id);
+      return;
+    }
+    // -- DATE
+    if (['created', 'updated'].includes(key)) {
+      RESULT.push(m_formatDate(edge[key]));
+      return;
+    }
+    // Else, normal processing
+    if (edge.hasOwnProperty(key)) RESULT.push(`"${edge[key]}"`); // enclose in quotes to support commas
+  })
+  return RESULT;
+}
+
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+/*/ Returns an array of node records
+    e.g. [ [<tacitus>], [<marc antony>], ...]
+/*/
+function m_GenerateEdgesArray(edges, edgekeys) {
+  /// Define Edge KEYS
+  const edgeArr = [];
+  edges.forEach(e => edgeArr.push(m_getEdgeValues(e, edgekeys)));
+  return edgeArr;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+/// MODULE IMPORT / EXPORT METHODS ////////////////////////////////////////////
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+MOD.ExportData = () => {
+  const DATA = UDATA.AppState('FILTEREDD3DATA');
+  const { nodes, edges } = DATA;
+  let EXPORT = '';
+
+  /// 1. Export Nodes
+  /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  /// Define Node KEYS
+  const nodekeys = [
+    'id',
+    'label',
+    { 'attributes': ['Node_Type', 'Extra Info', 'Notes'] },
+    'degrees',
+    { 'meta': ['created', 'updated']}
+  ];
+  const nodesArr = m_GenerateNodesArray(nodes, nodekeys);
+
+  /// 2. Export Edges
+  /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  /// Define Node KEYS
+  const edgekeys = [
+    'id',
+    'source',
+    'target',
+    { 'attributes': ['Relationship', 'Info', 'Citations', 'Category', 'Notes'] },
+    { 'meta': ['created', 'updated']}
+  ];
+  const edgesArr = m_GenerateEdgesArray(edges, edgekeys);
+
+  /// 3. Expand to CSV
+  /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  ///    3.1. NODES
+  ///    3.1.1. Create headers
+  const nodeHeadersArr = nodekeys.map(key => {
+    // eslint-disable-next-line prefer-reflect
+    if (Object.prototype.toString.call(key) === '[object Object]') {
+      const subKeys = Object.keys(key); // can have multiple subKeys
+      return subKeys.map(sk => key[sk].map(k => `${sk}:${k}`)).flat();
+    } else {
+      return key;
+    }
+  });
+  const nodeHeaders = nodeHeadersArr.flat();
+  nodesArr.unshift(nodeHeaders); // add headers
+  ///    3.1.2. Expand Nodes to CSV
+  const commaDelimitedNodes = nodesArr.map(n => n.join(','));
+  EXPORT += 'NODES\n';
+  EXPORT += commaDelimitedNodes.join('\n')
+
+  ///   3.2. EDGES
+  ///   3.2.1. Create headers
+  const edgeHeadersArr = edgekeys.map(key => {
+    // eslint-disable-next-line prefer-reflect
+    if (Object.prototype.toString.call(key) === '[object Object]') {
+      const subKeys = Object.keys(key); // can have multiple subKeys
+      return subKeys.map(sk => key[sk].map(k => `${sk}:${k}`)).flat();
+    } else {
+      return key;
+    }
+  });
+  const edgeHeaders = edgeHeadersArr.flat();
+  edgesArr.unshift(edgeHeaders); // add headers
+  ///   3.2.2 Expand Edges to CSV
+  const commaDelimitedEdges = edgesArr.map(e => e.join(','));
+  EXPORT += '\n\nEDGES\n';
+  EXPORT += commaDelimitedEdges.join('\n');
+
+  /// 4. Save to File
+  /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  // const encodedURI = encodeURI(EXPORT);
+  const link = document.createElement('a');
+  const blob = new Blob(["\ufeff", EXPORT]);
+  const url = URL.createObjectURL(blob);
+  link.href = url;
+  const DATASET = window.NC_CONFIG.dataset || "netcreate";
+  link.download = `${DATASET}_export.csv`;
+  // link.setAttribute('href', encodedURI);
+  // link.setAttribute('download', 'netcreate_export.csv');
+  document.body.appendChild(link); // Required for FF
+  link.click();
+  document.body.removeChild(link);
+}
+
+/// EXPORT CLASS DEFINITION ///////////////////////////////////////////////////
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+module.exports = MOD;
