46a842f729def3870a6f865ae4dfcef16ae66fd4
Ben Loh <ben@inquirium.net>
2022-03-02 15:47:14 -0800

    import: Add UI for selecting and managing import files.

--- build/app/view/netcreate/components/ImportExport.jsx
+++ build/app/view/netcreate/components/ImportExport.jsx
@@ -26,28 +26,102 @@ const UNISYS   = require('unisys/client');
 class ImportExport extends UNISYS.Component {
   constructor (props) {
     super(props);
-    this.state = {isExpanded: true};
-    this.onSelectExportNodes = this.onSelectExportNodes.bind(this);
-    this.onSelectExportEdges = this.onSelectExportEdges.bind(this);
-    this.onSelectImportNodes = this.onSelectImportNodes.bind(this);
-    this.onSelectImportEdges = this.onSelectImportEdges.bind(this);
+    this.state = {
+      isExpanded: true,
+      nodefile: undefined,
+      nodefileStatus: 'Select a node .csv file to import',
+      nodefileErrors: undefined,
+      edgefile: undefined,
+      edgefileStatus: 'Select an edge .csv file to import',
+      edgefileErrors: undefined,
+    };
+    this.onNodesExportSelect = this.onNodesExportSelect.bind(this);
+    this.onEdgesExportSelect = this.onEdgesExportSelect.bind(this);
+    this.onNodeImportFileSelect = this.onNodeImportFileSelect.bind(this);
+    this.onEdgeImportFileSelect = this.onEdgeImportFileSelect.bind(this);
+    this.onDoImport = this.onDoImport.bind(this);
 
     UDATA = UNISYS.NewDataLink(this);
   } // constructor
 
   /// UI EVENT HANDLERS /////////////////////////////////////////////////////////
   /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  onSelectExportNodes() { UDATA.LocalCall('EXPORT_NODES'); }
-  onSelectExportEdges() { UDATA.LocalCall('EXPORT_EDGES'); }
+  onNodesExportSelect() { UDATA.LocalCall('EXPORT_NODES'); }
+  onEdgesExportSelect() { UDATA.LocalCall('EXPORT_EDGES'); }
 
-  onSelectImportNodes() { }
+  onNodeImportFileSelect(e) {
+    const nodefile = e.target.files[0];
+    UDATA.LocalCall('VALIDATE_NODEFILE', { nodefile })
+      .then(result => {
+        if (result.isValid) {
+          this.setState({
+            nodefile,
+            nodefileStatus: "Ready for import",
+            nodefileErrors: undefined
+          });
+        } else {
+          const errorMsg = (
+            <div>
+              <div>Missing keys: {result.missingKeys.join(', ')}</div>
+              <div>Keys found in file: {result.fileKeys.join(', ')}</div>
+            </div>
+          );
+          this.setState({
+            nodefile: undefined,
+            nodefileStatus: 'Invalid nodes csv file!!!',
+            nodefileErrors: errorMsg
+          });
+        }
+      });
+  }
 
-  onSelectImportEdges() { }
+  onEdgeImportFileSelect(e) {
+    const edgefile = e.target.files[0];
+    UDATA.LocalCall('VALIDATE_EDGEFILE', { edgefile })
+      .then(result => {
+        if (result.isValid) {
+          this.setState({
+            edgefile,
+            edgefileStatus: "Ready for import",
+            edgefileErrors: undefined
+          });
+        } else {
+          const errorMsg = (
+            <div>
+              <div>Missing keys: {result.missingKeys.join(', ')}</div>
+              <div>Keys found in file: {result.fileKeys.join(', ')}</div>
+            </div>
+          );
+          this.setState({
+            edgefile: undefined,
+            edgefileStatus: "Invalid edges csv file!!!",
+            edgefileErrors: errorMsg
+          });
+        }
+      });
+  }
+
+  onDoImport() {
+    UDATA.LocalCall('UI_CLOSE_MORE'); // InfoPanel.jsx
+    UDATA.LocalCall('IMPORT'); // nc-logic -> export-logic
+    this.setState({ nodefile: undefined, edgefile: undefined }); // clear files for next import
+  }
+
 
 /// REACT LIFECYCLE METHODS ///////////////////////////////////////////////////
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
   render() {
+    const { nodefile,
+      nodefileStatus,
+      nodefileErrors,
+      edgefile,
+      edgefileStatus,
+      edgefileErrors
+    } = this.state;
+
+    const importDisabled = false;
+
     return (
       <div
         style={{
@@ -57,25 +131,32 @@ class ImportExport extends UNISYS.Component {
       >
         <h1>Export Data</h1>
 
-        <Button size="sm" outline onClick={this.onSelectExportNodes}>
+        <i className="small text-muted">Export data in .csv format.</i><br/>
+        <Button size="sm" outline onClick={this.onNodesExportSelect}>
           Export Nodes
         </Button>&nbsp;
-        <Button size="sm" outline onClick={this.onSelectExportEdges}>
+        <Button size="sm" outline onClick={this.onEdgesExportSelect}>
           Export Edges
         </Button>&nbsp;
-        <i className="small text-muted">Export data in .csv format.</i>
 
         <hr />
 
         <h1>Import Data</h1>
 
-        <Button size="sm" outline disabled onClick={this.onSelectExportNodes}>
-          Import Nodes
+        <i className="small text-muted">Import .csv data</i><br/>
+        <label>
+          <input type="file" accept="text/csv" id="nodefileInput" onChange={this.onNodeImportFileSelect}/>
+          &nbsp;<i>{nodefileStatus}</i><br />
+          {nodefileErrors && <span style={{ color: "red" }}>{nodefileErrors}</span>}
+        </label><br />
+        <label>
+          <input type="file" accept="text/csv" id="edgefileInput" onChange={this.onEdgeImportFileSelect}/>
+          &nbsp;<i>{edgefileStatus}</i><br />
+          {edgefileErrors && <span style={{ color: "red" }}>{edgefileErrors}</span>}
+        </label><br />
+        <Button size="sm" outline color={importDisabled ? "light" : "primary"} disabled={importDisabled} onClick={this.onDoImport}>
+          Import
         </Button>&nbsp;
-        <Button size="sm" outline disabled onClick={this.onSelectExportNodes}>
-          Import Edges
-        </Button>&nbsp;
-        <i className="small text-muted">Import .csv data</i>
       </div>
     );
   }
