cde2c0ed9d13c462fffd78f2506e98769b8680a0
DSri Seah <david@davidseah.com>
2023-07-30 20:03:12 -0400

    ur-modules: formally define NetCreate graph data in one place (doc, prep for future)
    NetCreate 2018 does not have a single obvious place where the Node and Edge structures are defined. It currently exists in the EdgeEditor and NodeSelector UI handler code...yikes!

--- _ur/_lib/validate.js
+++ _ur/_lib/validate.js
@@ -1,41 +1,6 @@
 /*///////////////////////////////// ABOUT \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*\
 
-  description'
-
-let node = {
-  label : formData.label?formData.label:'',
-  id: formData.id,
-  type: formData.type,
-  info: formData.info,
-  provenance: formData.provenance,
-  comments: formData.comments,
-  notes: formData.notes,
-  degrees: formData.degrees
-};
-
-id is number
-label is string
-type is string
-notes is string
-info is string
-provenance is string
-comments is string
-degrees is string this might be a bug
-created is string (metadata)
-updated is string (metadata)
-revision is string (metadata)
-
-id is number
-source is string (bug? is this referring to a node label or an id???)
-target is string (bug? is this referring to a node label or id???)
-type is string
-notes is string
-info is string
-weight is number
-provenance is string
-comments is string
-citation is string
-
+  WIP future home of realtime data validation
 
 \*\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ * /////////////////////////////////////*/
 
