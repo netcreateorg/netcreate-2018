dd42e6816b035d83cfde299afe6bd08dddc9074a
DSri Seah <david@davidseah.com>
2023-08-01 18:07:11 -0400

    ur-modules: new 'declare-actions' for action verbs, add to 'declare-codestyle'

--- _ur/_lib/declare-codestyle.js
+++ _ur/_lib/declare-codestyle.js
@@ -69,6 +69,97 @@ function m_Foo() {
   }
 }
 
+/// GENERAL FUNCTION NAMING ///////////////////////////////////////////////////
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+/*/
+    Synchronous functions can follow the existing convention but use an
+    'immediate' verb in the name like 'get/set' for simple data setting.
+    The function should be SIDE EFFECT FREE.
+/*/
+function m_GetSomething() {}
+function m_SetSomething() {}
+/*/
+    For API methods that operate on common kind of data structure
+    or modeled concept, use verbs that use the nomenclature in
+    use instead of just using Get/Set and include the concept or object
+    that is being operated on. The implication is that you pick
+    memorable, unique, and short names in your model design or
+    mirror class/object declarations.
+/*/
+function QueueAction() {} // not SetAction
+function ExecuteAction() {} // looks like it's related to QueueAction
+/*/
+    Asynchronous functions should use a 'waiting' type verb in the name
+    (e.g. AwaitSomething, PromiseDoAThing) to imply that the call is asynchronous. \
+    Can also use `ma_` prefix for module functions.
+/*/
+async function m_AwaitSomething() {}
+async function ma_ProcessTaskList() {}
+function API_PromiseLoadConfig() {}
+/*/
+    At times you'll be working on a big messy prototype function.
+    If you plan on making intermediate "progress" commits to the repo,
+    consider naming those big messy functions as WIP_.
+/*/
+function WIP_DoSomething() {}
+
+/// ASYNC CALLBACKS & EVENTS //////////////////////////////////////////////////
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+/*/
+    Handlers that are fired as a callback should be named to imply
+    that they are delivering a result. I use cb or cb_ to make
+    this distinct from an event handler or functional parameter.
+    Note the use of snake_case, similar to inline functions.
+    If the callback has to do more work, have it call a higher-
+    level function or API method to keep the callback nice and small.
+/*/
+const cb_timer_expired = () => console.log('expired');
+setInterval(cb_timer_expired, 1000);
+/*/
+    Event handlers come in two flavors: as the immediate handler that
+    is registered or subscribed to a source, and the specific
+    work that is done. DO NOT COMBINE THEM. In general, your immediate
+    handlers should be short and call a higher-level function/method
+    that follows our general function naming conventions. This is the
+    old wrong way:
+
+      SubmitButton.subscribe('click', HandleClick);
+      // handle clicks
+      function HandleClick() {
+        // save stuff to database
+        const DB = GetDB();
+        const { data } = GetFormData();
+        DB.Write(data);
+        // notify subscribers
+        SYSTEM.NotifySubscribers(data);
+      }
+
+    This is sub-literate code because (1) function names reflect the
+    name of a UI event, not what the context of the button does and
+    (2) there are side effects that are not hinted by the function
+    name (3) these operations are not managed by a single module
+    where one would expect to find such operaetions.
+
+    Instead, split the things up using arrow functions that are
+    for DISPATCHING the event to an appropriate handler. This allows
+    you to centralize action processing. Talk to Sri for more examples
+/*/
+// this is a dummy declaration of an appcore module, which is
+// the only type of module that can talk to both UI and DATA
+// modules. It's equivalent to a controller in MVC.
+const APPCORE_DATASUBMIT = {
+  HandleFormSubmit: function (event, param) {
+    const { target } = event;
+    // determine what to do based on current state of application
+    // do the things
+  }
+};
+// this is a dummy component that is a UI element
+const DummySubmitButton = {};
+DummySubmitButton.on('click', event =>
+  APPCORE_DATASUBMIT.HandleFormSubmit(event, 'param')
+);
+
 /// CLASS DECLARATION /////////////////////////////////////////////////////////
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 /** class declarations are used instead of API METHODS. If you are
@@ -128,11 +219,11 @@ MyClass.staticProperty = 'bar';
  */
 function PublicA() {}
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-/** a function comment: note that the JSDOC styling here starts on the top line,
- *  and below a /// divider. The subsequent lines are aligned with the top
- *  line this improves readability with the straight left edge,
+/** alternatively, put the prefix API_ in the name to make it super-clear what
+ *  is intended for a public methods. Rename it as an export, though, as
+ *  module.exports = { PublicB: API_PublicB }
  */
-function PublicB() {}
+function API_PublicB() {}
 
 /// API: ANOTHER SECTION /////////////////////////////////////////////////////
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -167,7 +258,7 @@ function FeedFish() {
 /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 module.exports = {
   PublicA,
-  PublicB,
+  PublicB: API_PublicB,
   MyClass
 };
 
